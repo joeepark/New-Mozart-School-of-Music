c82ffb472b098d7c3030b5cf21486cf57bf64cb8
JosephPark <joeepark@gmail.com>
2022-12-12 15:02:41 -0800

    Configuring SQL query to add classrooms and display it on the classroom table

--- pages/api/classrooms.js
+++ pages/api/classrooms.js
@@ -33,9 +33,9 @@ async function addClassroom(req, res) {
   try {
     const jsonData = req.body;
     const classroomData = JSON.parse(jsonData);
-    const { name, schedule_id, student_id, teacher_id } = classroomData;
- 
-    const response = await pool.query(query, [name, schedule_id, student_id, teacher_id]);
+    const { name } = classroomData;
+    const query = `INSERT INTO classrooms (name) VALUES ($1)`
+    const response = await pool.query(query, [name]);
     return res.status(200).json(response.rows);
   } catch (err) {
     return res.status(500).json({ error: err.message });
