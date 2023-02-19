397d9f90da5360651243aa552e75902da501f9da
JosephPark <joeepark@gmail.com>
2023-02-18 18:52:34 -0800

    Finished for the day with a final version that I am happy with

--- client/components/overview/Overview.jsx
+++ client/components/overview/Overview.jsx
@@ -1,10 +1,34 @@
 function Overview() {
   return (
     <section className="overview">
-      <div className="announcements-container">Announcements</div>
-      <div className="upcoming-events-container">Upcoming</div>
-      <div className="tuition-rates container">Tuition Rates</div>
-      <div className="holidays-container">Holidays</div>
+      <div className="announcements-container">
+        <h3>Announcements</h3>
+        <p>Check out the new app for us to use! I hope you guys are doing well.</p>
+      </div>
+      <div className="upcoming-events-container">
+        <h3>Upcoming</h3>
+        <p>New Mozart Faculty Recital 2/26, Winter Student Recital 1/15 and 1/22.</p>
+      </div>
+      <div className="tuition-rates container">
+        <h3>Tuition Rates</h3>
+        <p>
+          Monthly Rate: $240, 260, $315 <br />
+          Per Lesson Rate: $64, $69, $84
+        </p>
+      </div>
+      <div className="holidays-container">
+        <h3>Holidays</h3>
+        <p>
+          Independence Day Break: July 1-4 <br />
+          Labor Day Break: Aug 30 - Sep 5 <br />
+          Thanksgiving Break: Nov 22-27 <br />
+          Winter Break: Dec 22 - Jan 3 <br />
+          Staff Development: Mar 1-3 <br />
+          Spring Break: Apr 3-9 <br />
+          Memorial Day: May 29-31 <br />
+          June Break: June 12-18
+        </p>
+      </div>
     </section>
   );
 }
